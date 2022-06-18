1b9ce888e83b2403b42151d11f49121410eecefa
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-06-18 23:32:19 +0630

    stable version 3.0

--- lua/user/lsp/settings/emmet_ls.lua
+++ lua/user/lsp/settings/emmet_ls.lua
@@ -1,6 +1,12 @@
 -- https://github.com/pedro757/emmet
 -- npm i -g ls_emmet
-require("lspconfig").emmet_ls.setup({
-  cmd = { "ls_emmet", "--stdio" },
-  filetypes = { "html" },
-})
+-- require("lspconfig").emmet_ls.setup({
+--   cmd = { "ls_emmet", "--stdio" },
+--   filetypes = { "html" },
+-- })
+return {
+	cmd = { "ls_emmet", "--stdio" },
+	filetypes = {
+		"html",
+	},
+}
