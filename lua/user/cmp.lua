3f06acfccdd00199e373202a4dfc0b7af69f5882
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-07-17 17:49:27 +0630

    IDE finalize

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -8,16 +8,13 @@ if not snip_status_ok then
 	return
 end
 
+local compare = require("cmp.config.compare")
+
 require("luasnip/loaders/from_vscode").lazy_load()
 require("luasnip").filetype_extend("javascript", { "javascriptreact" })
 require("luasnip").filetype_extend("typescript", { "typescriptreact" })
 
 local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
-local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
@@ -55,11 +52,14 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.jumpable(1) then
+				luasnip.jump(1)
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
+			elseif luasnip.expandable() then
+				luasnip.expand()
 			elseif check_backspace() then
+				-- cmp.complete()
 				fallback()
 			else
 				fallback()
@@ -71,9 +71,9 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-				-- elseif luasnip.jumpable(-1) then
-				-- 	luasnip.jump(-1)
-				-- else
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
 				fallback()
 			end
 		end, {
@@ -87,9 +87,6 @@ cmp.setup({
 			-- Kind icons
 			vim_item.kind = kind_icons[vim_item.kind]
 
-			if entry.source.name == "cmp_tabnine" then
-				vim_item.kind = icons.misc.Robot
-			end
 			if entry.source.name == "copilot" then
 				vim_item.kind = icons.git.Octoface
 				vim_item.kind_hl_group = "CmpItemKindCopilot"
@@ -112,14 +109,34 @@ cmp.setup({
 		end,
 	},
 	sources = {
-		{ name = "copilot" },
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "cmp_tabnine" },
-		{ name = "path" },
-		{ name = "emoji" },
+		{ name = "crates", group_index = 1 },
+		{ name = "nvim_lsp", group_index = 2 },
+		{ name = "nvim_lua", group_index = 2 },
+		{ name = "copilot", group_index = 2 },
+		{ name = "luasnip", group_index = 2 },
+		{ name = "buffer", group_index = 2 },
+		{ name = "cmp_tabnine", group_index = 2 },
+		{ name = "path", group_index = 2 },
+		{ name = "emoji", group_index = 2 },
+	},
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			-- require("copilot_cmp.comparators").prioritize,
+			-- require("copilot_cmp.comparators").score,
+			compare.offset,
+			compare.exact,
+			-- compare.scopes,
+			compare.score,
+			compare.recently_used,
+			compare.locality,
+			-- compare.kind,
+			compare.sort_text,
+			compare.length,
+			compare.order,
+			-- require("copilot_cmp.comparators").prioritize,
+			-- require("copilot_cmp.comparators").score,
+		},
 	},
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
@@ -132,7 +149,7 @@ cmp.setup({
 		},
 		completion = {
 			border = "rounded",
-			-- winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
 		},
 	},
 	experimental = {
