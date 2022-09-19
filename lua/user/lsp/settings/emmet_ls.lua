55c0a0b35d353dbbb8ca60f57c27049950671436
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-09-19 19:44:38 +0630

    nvim-upgraged

---
+++ lua/user/lsp/settings/emmet_ls.lua
@@ -0,0 +1,16 @@
+-- https://github.com/pedro757/emmet
+-- npm i -g ls_emmet
+return {
+  cmd = { "ls_emmet", "--stdio" },
+  filetypes = {
+    "html",
+    "haml",
+    "xml",
+    "xsl",
+    "pug",
+    "slim",
+    "stylus",
+    "hbs",
+    "handlebars",
+  },
+}
